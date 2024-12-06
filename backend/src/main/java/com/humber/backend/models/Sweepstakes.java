7498bbe03490ddb91eb3eac015ff6d612054e7b5
andrew <andrew@bobux.com>
2024-12-06 17:24:24 -0500

    defined MyUser and Sweepstakes

---
+++ backend/src/main/java/com/humber/backend/models/Sweepstakes.java
@@ -0,0 +1,23 @@
+package com.humber.FinalProjectCPAN228.models;
+
+import org.springframework.data.annotation.Id; // Correct import for MongoDB
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "sweepstakes")
+public class Sweepstakes {
+    @Id
+    private String id;
+    private String itemId;
+    private boolean isActive;
+    private LocalDateTime expirationDate;
+    private List<String> entries;
+}
