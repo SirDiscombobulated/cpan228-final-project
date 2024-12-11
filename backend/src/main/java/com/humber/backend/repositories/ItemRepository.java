0717d0ca362679e80a065b0eef37b2d03d35a439
loltommy56 <72711069+loltommy56@users.noreply.github.com>
2024-12-10 20:54:28 -0500

    changes

---
+++ backend/src/main/java/com/humber/backend/repositories/ItemRepository.java
@@ -0,0 +1,15 @@
+package com.humber.backend.repositories;
+
+import com.humber.backend.models.Item;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("mongoItemRepository") // Explicit name for MongoDB repository
+public interface ItemRepository extends MongoRepository<Item, String> {
+
+   
+    public List<Item> findByIgnoreCaseCategoryAndPrice(String category, Double price);
+
+}
