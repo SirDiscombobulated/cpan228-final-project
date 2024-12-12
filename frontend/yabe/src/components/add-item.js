8de0f39161c78ca8643dcd66a3d2cf4bc49a5c32
andrew <andrew@bobux.com>
2024-12-11 23:17:14 -0500

    updated the css for add-item & changed featured items from 9 to 8

--- frontend/yabe/src/components/add-item.js
+++ frontend/yabe/src/components/add-item.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { getAuthHeader, sendData } from './auth/auth';
+import "./styling/add-item.css";
 
 const AddItemPage = ({ itemId }) => {
     const [item, setItem] = useState({
