5d41ffb43c5261c0c017f9cd44a02da828ea1870
andrew <andrew@bobux.com>
2024-12-11 19:29:46 -0500

    updated ItemController to automatically filter for available items

--- backend/src/main/java/com/humber/backend/repositories/ItemRepository.java
+++ backend/src/main/java/com/humber/backend/repositories/ItemRepository.java
@@ -1,7 +1,9 @@
 package com.humber.backend.repositories;
 
 import com.humber.backend.models.Item;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,15 @@ import java.util.List;
 // interface that provides a set of CRUD operations for MongoDB documents
 public interface ItemRepository extends MongoRepository<Item, String> {
     List<Item> findByIgnoreCaseCategoryAndPrice(String category, Double price);
+
+    List<Item> findByIgnoreCaseTitleContaining(String title);
+
+    @Aggregation(pipeline = {
+            "{ '$project': { 'id': 1, 'title': 1, 'category': 1, 'price': 1, 'description': 1, 'createdAt': 1, 'status': 1, 'ownerId': 1, 'interested': 1, 'interestedCount': { $size: '$interested' } } }",
+            "{ '$sort': { 'interestedCount': -1 } }",
+            "{ '$limit': 9 }"})
+    List<Item> findTopInterestedItems();
+
+    List<Item> findByIgnoreCaseStatusContainingAndIgnoreCaseTitleContaining(String status, String title);
+
 }
