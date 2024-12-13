5d483e48ff5d349bfc2f4b4382abefa269ae1baa
andrew <andrew@bobux.com>
2024-12-12 22:50:56 -0500

    removed unused imports

--- backend/src/main/java/com/humber/backend/services/ItemService.java
+++ backend/src/main/java/com/humber/backend/services/ItemService.java
@@ -3,10 +3,6 @@ package com.humber.backend.services;
 import com.humber.backend.models.Item;
 import com.humber.backend.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
