3e435db5a78faecfd1cd00072337ef9a8b88026b
loltommy56 <72711069+loltommy56@users.noreply.github.com>
2024-12-12 23:23:27 -0500

    remove admin in nav bar

--- frontend/yabe/src/components/fragments/navbar.js
+++ frontend/yabe/src/components/fragments/navbar.js
@@ -7,7 +7,6 @@ const Header = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <div className="navbar-nav d-flex ms-auto">
                     <Link className="nav-link" to="/store/stock">Stock</Link>
-                    <Link className="nav-link" to="/store/admin/">Admin</Link>
                     <Link className="nav-link" to="/store/admin/add-item">Add Items</Link>
                 </div>
             </div>
